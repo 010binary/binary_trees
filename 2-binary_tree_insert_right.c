d739b7c21bb196f83a383637db9557cf94653523
010binary <augustine32fg@gmail.com>
2024-03-01 12:48:24 +0100

    binary tree preorder

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,32 +2,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Inserts a node as
- * the right-child of another node
- * @parent: Pointer to the node to insert the right-child in
- * @value: Value to store in the new node
- *
- * Return: Pointer to the created node,
- * or NULL on failure or if parent is NULL
+ * binary_tree_insert_right - inserts a node as the right of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to put in the new node
+ * Return: pointer to created node
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *newNode;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
+	newNode = binary_tree_node(parent, value);
+	if (newNode == NULL)
 		return (NULL);
 
 	if (parent->right != NULL)
 	{
-		new_node->right = parent->right;
-		new_node->right->parent = new_node;
+		newNode->right = parent->right;
+		newNode->right->parent = newNode;
 	}
 
-	parent->right = new_node;
+	parent->right = newNode;
 
-	return (new_node);
+	return (newNode);
 }
--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,10 +2,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - Deletes an entire binary tree
- * @tree: Pointer to the root node of the tree to delete
- *
- * If tree is NULL, do nothing
+ * binary_tree_delete - Deletes a binary tree.
+ * @tree: A pointer to the root node of the tree to delete.
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
