0b470e041681817d575bc7516a4ef784234c5a63
010binary <augustine32fg@gmail.com>
2024-03-01 10:48:40 +0100

    1-binary with abstration

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,13 +11,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     binary_tree_t *temp;
 
     newNode = malloc(sizeof(binary_tree_t));
-
     if (newNode == NULL)
-        return (NULL);
+            return (NULL);
 
     newNode->n = value;
     newNode->left = NULL;
     newNode->parent = parent;
     newnode->right = NULL;
+    
     return (newNode);
 }
